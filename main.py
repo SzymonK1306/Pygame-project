aee709a3d70460ecd716fa12a71e6de236cb7916
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-01-31 23:51:40 +0100

    add simple animation of snail

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 from sys import exit
 
-pygame.init()
+pygame.init()       # initiation pygame
 window = pygame.display.set_mode((800, 400))
-pygame.display.set_caption("Runner")
+pygame.display.set_caption("Runner")    # title of the window
 clock = pygame.time.Clock()     # clock object
-testFont = pygame.font.Font("fonts/Pixeltype.ttf", 50)
+font = pygame.font.Font("fonts/Pixeltype.ttf", 50)  # font object
 
-sky = pygame.image.load("images/Sky.png")
-ground = pygame.image.load("images/ground.png")
-testSurface = testFont.render("My game", False, "Black")
+sky_surface = pygame.image.load("images/Sky.png").convert()     # convert is for our game run faster
+ground_surface = pygame.image.load("images/ground.png").convert()
+textSurface = font.render("My game", False, "Black")
+snail_surface = pygame.image.load("images/snail/snail1.png").convert_alpha()
+snail_xPos = 700
+
 
 
 while True:
@@ -19,9 +22,13 @@ while True:
             pygame.quit()
             exit()      # closing program without the error
 
-    window.blit(sky, (0, 0))
-    window.blit(ground, (0, 300))
-    window.blit(testSurface, (300, 50))
+    window.blit(sky_surface, (0, 0))
+    window.blit(ground_surface, (0, 300))
+    window.blit(textSurface, (300, 50))
+    snail_xPos -= 4
+    if snail_xPos < -100:
+        snail_xPos = 700
+    window.blit(snail_surface, (snail_xPos, 265))
 
     pygame.display.update()
     clock.tick(60)      # maximum frame rate
