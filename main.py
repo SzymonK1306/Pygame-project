ff05af1a4d4adf9785ac1bb8052f6d1c9ee77494
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-02-01 12:41:50 +0100

    add player surface, rectangles and update variables names

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ pygame.display.set_caption("Runner")    # title of the window
 clock = pygame.time.Clock()     # clock object
 font = pygame.font.Font("fonts/Pixeltype.ttf", 50)  # font object
 
-sky_surface = pygame.image.load("images/Sky.png").convert()     # convert is for our game run faster
-ground_surface = pygame.image.load("images/ground.png").convert()
-textSurface = font.render("My game", False, "Black")
-snail_surface = pygame.image.load("images/snail/snail1.png").convert_alpha()
-snail_xPos = 700
+sky_surf = pygame.image.load("images/Sky.png").convert()     # convert is for our game run faster
 
+ground_surf = pygame.image.load("images/ground.png").convert()
+
+text_surf = font.render("My game", False, "Black")
+
+snail_surf = pygame.image.load("images/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+player_surf = pygame.image.load("images/player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 
 while True:
@@ -22,13 +27,16 @@ while True:
             pygame.quit()
             exit()      # closing program without the error
 
-    window.blit(sky_surface, (0, 0))
-    window.blit(ground_surface, (0, 300))
-    window.blit(textSurface, (300, 50))
-    snail_xPos -= 4
-    if snail_xPos < -100:
-        snail_xPos = 700
-    window.blit(snail_surface, (snail_xPos, 265))
+    window.blit(sky_surf, (0, 0))
+    window.blit(ground_surf, (0, 300))
+    window.blit(text_surf, (300, 50))
+
+    snail_rect.x -= 4
+    if snail_rect.right < -100:
+        snail_rect.left = 800
+
+    window.blit(snail_surf, snail_rect)
+    window.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)      # maximum frame rate
