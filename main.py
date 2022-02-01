0723c75bd5ab1faf0bea85b9ad863be1d6d570f5
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-02-01 17:45:32 +0100

    change score background color, change font color, simple colision

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ sky_surf = pygame.image.load("images/Sky.png").convert()     # convert is for ou
 
 ground_surf = pygame.image.load("images/ground.png").convert()
 
-text_surf = font.render("My game", False, "Black")
+score_surf = font.render("Score:", False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load("images/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom=(600, 300))
@@ -26,10 +27,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()      # closing program without the error
+        if event.type == pygame.MOUSEMOTION:    # checking mouse collision
+            if player_rect.collidepoint(event.pos):
+                player_rect.bottom = 250
+            else:
+                player_rect.bottom = 300
 
     window.blit(sky_surf, (0, 0))
     window.blit(ground_surf, (0, 300))
-    window.blit(text_surf, (300, 50))
+    pygame.draw.rect(window, "#c0e8ec", score_rect)
+    window.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right < -100:
@@ -38,5 +45,7 @@ while True:
     window.blit(snail_surf, snail_rect)
     window.blit(player_surf, player_rect)
 
+    if player_rect.colliderect(snail_rect):     # return 1 when is a collision
+        snail_rect.left = 800
     pygame.display.update()
     clock.tick(60)      # maximum frame rate
