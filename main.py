d222a891d93e2bb7623db4aa04a5787fd1b06d25
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-02-02 19:44:27 +0100

    add function to display score

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks()/1000) - start_time
+    score_surf = font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    window.blit(score_surf, score_rect)
+
 pygame.init()       # initiation pygame
 window = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")    # title of the window
@@ -8,13 +14,11 @@ clock = pygame.time.Clock()     # clock object
 font = pygame.font.Font("fonts/Pixeltype.ttf", 50)  # font object
 
 game_active = True
+start_time = 0
 
 sky_surf = pygame.image.load("images/Sky.png").convert()     # convert is for our game run faster
 
 ground_surf = pygame.image.load("images/ground.png").convert()
-
-score_surf = font.render("Score:", False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load("images/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom=(600, 300))
@@ -44,13 +48,14 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                start_time = int(pygame.time.get_ticks()/1000)
 
     if game_active:
         window.blit(sky_surf, (0, 0))
         window.blit(ground_surf, (0, 300))
-        pygame.draw.rect(window, "#c0e8ec", score_rect)
-        window.blit(score_surf, score_rect)
+        display_score()
 
+        # snail movement
         snail_rect.x -= 4
         if snail_rect.right < -100:
             snail_rect.left = 800
@@ -77,7 +82,6 @@ while True:
             snail_rect.right = 800
     else:
         window.fill("Pink")
-
 
     pygame.display.update()
     clock.tick(60)      # maximum frame rate
