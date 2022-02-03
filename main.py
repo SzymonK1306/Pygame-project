dadda70c97b18bc57caa10265129e1d6ee346826
Szymon Kryzel <szymonkryzel13@gmail.com>
2022-02-03 20:31:44 +0100

    add player animation

--- main.py
+++ main.py
@@ -34,6 +34,22 @@ def collision(player, obstacles):
     return True
 
 
+def player_animation():
+    global player_index
+    global player_surf
+    # walking while player is on the floor
+    # jumping while player is not on the floor
+    if player_rect.bottom < 300:
+        player_surf = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk):
+            player_index = 0
+        player_surf = player_walk[int(player_index)]
+
+
+
+
 pygame.init()  # initiation pygame
 window = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")  # title of the window
@@ -54,7 +70,12 @@ fly_surf = pygame.image.load("images/Fly/Fly1.png").convert_alpha()
 
 obstacle_rect_list = []
 
-player_surf = pygame.image.load("images/player/player_walk_1.png").convert_alpha()
+player_walk1 = pygame.image.load("images/player/player_walk_1.png").convert_alpha()
+player_walk2 = pygame.image.load("images/player/player_walk_2.png").convert_alpha()
+player_walk = [player_walk1, player_walk2]
+player_index = 0
+player_jump = pygame.image.load("images/player/jump.png").convert_alpha()
+player_surf = player_walk[player_index]
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 # intro screen
@@ -110,6 +131,7 @@ while True:
         player_rect.y += gravity
         if player_rect.bottom >= 300:
             player_rect.bottom = 300
+        player_animation()
         window.blit(player_surf, player_rect)
 
         # Obstacle movement
